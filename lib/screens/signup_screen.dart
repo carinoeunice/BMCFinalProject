0d1295b1eefd770a2c1eb53e8999048bd5394d11
CarinoEunice <ecarino021920@gmail.com>
2025-10-30 17:56:26 +0800

    Flutter 3.32.5 V Firebase Integration Part 1

---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:ecommerce_app/screens/signup_screen.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+
+  class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  }
+}
